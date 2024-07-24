300cca55e0a5a3cd22fbea5fd7655309ad232cc6
Wbcom Dev <pallavi@wbcomdesigns.com>
2024-07-24 16:30:30 +0530

    Fixed issue with buddyboss

--- public/class-buddypress-share-public.php
+++ public/class-buddypress-share-public.php
@@ -171,7 +171,11 @@ class Buddypress_Share_Public {
 		$social_service = get_site_option( 'bp_share_services' );
 		$extra_options  = get_site_option( 'bp_share_services_extra' );
 		$activity_type  = bp_get_activity_type();
-		$activity_link  = $activities_template->activity->primary_link . bp_get_activity_slug() . '/' . $activities_template->activity->id . '/';
+		if( ( function_exists( 'buddypress' ) && isset( buddypress()->buddyboss  ) ) ){
+			$activity_link = site_url(). '/' . bp_get_members_slug() . '/' . $activities_template->activity->display_name . '/' .  bp_get_activity_slug() . '/' . $activities_template->activity->id . '/';
+		} else {
+			$activity_link  = $activities_template->activity->primary_link . bp_get_activity_slug() . '/' . $activities_template->activity->id . '/';
+		}
 		$activity_title = bp_get_activity_feed_item_title(); // use for description : bp_get_activity_feed_item_description().
 		$plugin_path    = plugins_url();
 		if ( ! is_user_logged_in() ) {
